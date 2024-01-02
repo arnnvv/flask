30a3d9ffc26b070f19b1b19d039bbc5ac51523c9
arnnvv <arnav.900@protonmail.com>
2024-01-02 22:16:04 +0530

    dn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,68 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  function CreateComponent() {}
+  function Todos() {
+    const [todos, setTodos] = useState([
+      {
+        title: "GYM",
+        description: "Go to GYM from 5-7",
+      },
+      {
+        title: "DSA",
+        description: "DO DSA",
+      },
+    ]);
+
+    const [newTodo, setNewTodo] = useState({
+      title: "",
+      description: "",
+    });
+
+    const addTodo = () => {
+      setTodos((prevtodo) => [...prevtodo, newTodo]);
+    };
+
+    return (
+      <div id="todo-form">
+        <input
+          type="text"
+          placeholder="Title"
+          value={newTodo.title}
+          onChange={(e) => setNewTodo({ ...newTodo, title: e.target.value })}
+          className="w-full p-2 mb-4 border border-gray-300 rounded"
+        />
+        <input
+          type="text"
+          placeholder="Description"
+          value={newTodo.description}
+          onChange={(e) =>
+            setNewTodo({ ...newTodo, description: e.target.value })
+          }
+          className="w-full p-2 mb-4 border border-gray-300 rounded"
+        />
+        <button
+          onClick={addTodo}
+          className="bg-green-500 text-white py-2 px-4 rounded cursor-pointer"
+        >
+          Add Todo
+        </button>
+
+        <div id="todo-list">
+          {todos.map((todo, index) => (
+            <div key={index} className="todo-item">
+              <div>{todo.title}</div>
+              <div>{todo.description}</div>
+              <br />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 
   return (
     <>
-      <CreateComponent></CreateComponent>
+      <Todos></Todos>
     </>
   );
 }
